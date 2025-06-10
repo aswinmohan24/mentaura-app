589e9fe28237a91bb4cdbafc6a88560040eab2a4
Aswin Mohanan <aswinmohan242@gmail.com>
2025-06-10 11:58:48 +0530

    .env added

--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 const authRouter = require("./routes/auth");
 const emotionRouter = require("./routes/emotion");
+require('dotenv').config();
 const PORT = process.env.PORT | 3001;
-const DB = "mongodb+srv://wipergaming92:wiper9207@cluster0.sfguqjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use(authRouter)
 app.use(emotionRouter)
 
-mongoose.connect(DB).then(()=>{
+mongoose.connect(process.env.DB).then(()=>{
     console.log("Connection successfull")
 }).catch((err)=>{
     console.log(err);
